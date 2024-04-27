e020d24f4bc864e380a0d22498d0c5a68a610efa
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-04-27 18:46:43 -0300

    menu mobile e desktop

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const btMenu = document.querySelector('.menu_mobile');
+const cabecalho = document.querySelector('.cabecalho .container');
+const barrinhasMenu = document.querySelectorAll('.menubar');
 
 btMenu.addEventListener('click', ()=> {
     btMenu.classList.toggle('ativa');
+    if(!cabecalho.classList.contains('rolou')){
+        cabecalho.classList.toggle('rolou');
+    }
+})
+
+window.addEventListener('scroll', ()=> {
+    const itens = document.querySelectorAll('.lista_menu_itens'); 
+    cabecalho.classList.toggle('rolou', window.scrollY > 0);
+
+    barrinhasMenu.forEach( barrinha => {
+        barrinha.classList.toggle('menubar-ativo', window.scrollY > 0);
+    })
+
+    itens.forEach( item => {
+        item.classList.toggle('item-scroll', window.scrollY > 0);
+    })
 })
