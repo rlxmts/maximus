9543c453141023bd31b53a9c1051df7238adc940
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-13 16:43:05 -0300

    :bug: Consertando bug de nav subindo com o menu

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const btMobile = document.querySelector('.menu_mobile');
 const cabecalho = document.querySelector('.cabecalho');
 const btWpp = document.querySelector('.whatsapp_bt');
 const botaoSubir = document.querySelector('.botao-subir');
+const menu = document.querySelector('.menu_navegacao');
 
+
+//ANIMAÇÃO DO MENU MOBILE AO SER CLICADO
 btMobile.addEventListener('click', ()=> {
     btMobile.classList.toggle('ativo');
+    cabecalho.classList.toggle('fixar');
 })
 
+//ALTERANDO BACKGROUND DO MENU AO ROLAR A PAGINA E TORNANDO VISIVEL BOTAO DE SUBIR AO TOPO.
 document.addEventListener('scroll', ()=> {
     if(scrollY > 10){
         cabecalho.classList.add('cabecalho-ativo');
@@ -23,8 +28,8 @@ document.addEventListener('scroll', ()=> {
     }
 })
 
+// TORNANDO O MENU RETRATIL, ESCONDE AO ROLAR PARA BAIXO, MOSTRAR AO ROLAR PARA CIMA
 let posicaoAntScroll = window.scrollY;
-
 window.addEventListener( 'scroll', ()=> {
     
     let posicaoAtualScroll = window.scrollY;
@@ -35,4 +40,9 @@ window.addEventListener( 'scroll', ()=> {
     }
 
     posicaoAntScroll = posicaoAtualScroll;
-})
+}); 
+
+
+
+
+    
