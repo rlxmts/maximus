5bffad4b463d2b68bca7c9b86508dd032e041232
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-13 16:14:55 -0300

    :dizzy: botao subir animation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btMobile = document.querySelector('.menu_mobile');
 const cabecalho = document.querySelector('.cabecalho');
 const btWpp = document.querySelector('.whatsapp_bt');
+const botaoSubir = document.querySelector('.botao-subir');
 
 btMobile.addEventListener('click', ()=> {
     btMobile.classList.toggle('ativo');
@@ -13,6 +14,12 @@ document.addEventListener('scroll', ()=> {
     }else{
         cabecalho.classList.remove('cabecalho-ativo');
         btWpp.classList.remove('bt-ativo');
+    }
+
+    if(scrollY > 1100){
+        botaoSubir.classList.add('visivel');
+    }else{
+        botaoSubir.classList.remove('visivel');
     }
 })
 
